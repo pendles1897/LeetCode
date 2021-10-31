4272bfcec7ff47a9eafae4520d4a2aec241952d0
William Pendleton <eugependleton@wills-laptop.home>
2021-11-01 10:17:43 +1100

    two-sum

--- buy_sell.py
+++ buy_sell.py
@@ -1,5 +1,7 @@
+from typing import List
+
 class Solution:
-    def maxProfit(self, prices: list[int]) -> int:
+    def maxProfit(self,prices:List[int]) -> int:
 
         profit = 0
         n = len(prices)
---
+++ two_sum.py
@@ -0,0 +1,13 @@
+from typing import List
+
+class Solution:
+    def twoSum(self, nums, target):
+        for a in nums:
+            b = target - a
+            if ((b in nums) == True and (nums.index(a) != nums.index(b))):
+                return[nums.index(a),nums.index(b)]
+nums = [2,4,6,8]
+target = 12
+
+combo1 = Solution()
+print(combo1.twoSum(nums,target))
