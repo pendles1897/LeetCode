9b22db43a2246c05906c98a2f14d0c51224814f4
William Pendleton <eugependleton@wills-laptop.home>
2021-11-01 08:07:16 +1100

    buy-sell

---
+++ buy_sell.py
@@ -0,0 +1,22 @@
+class Solution:
+    def maxProfit(self, prices: list[int]) -> int:
+
+        profit = 0
+        n = len(prices)
+        if n == 0: return profit
+        
+        buy_price = prices[0]
+        for i in range(1, n):
+            
+            if prices[i] < prices[i - 1] and prices[i - 1] > buy_price:
+                profit += prices[i - 1] - buy_price
+                buy_price = prices[i]
+                
+            elif prices[i] < prices[i - 1]:
+                buy_price = prices[i]
+                
+            # must sell at the end of the period
+            if i == n - 1 and prices[i] > buy_price:
+                profit += prices[i] - buy_price
+                
+        return profit
